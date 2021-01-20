532e59cd7be9df03ca4b11d051e788dbc66dd138
Annisa Purbandari <a.purbandari@gmail.com>
2021-01-20 14:40:58 +1100

    fix toggler navbar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,46 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import "./style.css";
 
 
-// Depending on the current path, this component sets the "active" class on the appropriate navigation link item
 function Navbar() {
+
+    const [isNavCollapsed, setIsNavCollapsed] = useState(true);
+
+    const handleNavCollapse = () => setIsNavCollapsed(!isNavCollapsed);
+
     return (
         <nav className="navbar navbar-b navbar-trans navbar-dark navbar-expand-md fixed-top">
             <Link className="navbar-brand" to="/">
                 Annisa
       </Link>
-            <div className="container">
-                <ul className="navbar-nav">
-                    <li className="nav-item">
-                        <Link
-                            to="/"
-                            className={
-                                window.location.pathname === "/" || window.location.pathname === "/about" ? "nav-link active" : "nav-link"}
-                        >
-                            About
-                        </Link>
-                    </li>
-                    <li className="nav-item">
-                        <Link
-                            to="/portfolio"
-                            className={window.location.pathname === "/portfolio" ? "nav-link active" : "nav-link"}
-                        >
-                            Portfolio
-                        </Link>
-                    </li>
 
-                    <li className="nav-item">
-                        <Link
-                            to="/contact"
-                            className={
-                                window.location.pathname === "/contact" ? "nav-link active" : "nav-link"}
-                        >
-                            Contact
-                        </Link>
-                    </li>
+            <button class="custom-toggler navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExample09" aria-controls="navbarsExample09" aria-expanded={!isNavCollapsed ? true : false} aria-label="Toggle navigation" onClick={handleNavCollapse}>
+                <span class="navbar-toggler-icon"></span>
+            </button>
 
-                </ul>
+            <div class={`${isNavCollapsed ? 'collapse' : ''} navbar-collapse`} id="navbarsExample09">
+                <a className="nav-link text-info" href="/">About</a>
+                <a className="nav-link text-info" href="/portfolio">Portfolio</a>
+                <a className="nav-link text-info" href="/contact">Contact</a>
             </div>
         </nav>
     );
